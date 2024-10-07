b7d9aefa946fe18acd492bf651d7181bcf72134f
shogo0421 <shibuya.shogo@classmethod.jp>
2024-10-07 17:29:47 +0900

    cdk Stack作成

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,9 +1,32 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const websiteBucket = new cdk.aws_s3.Bucket(this, "websiteBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      bucketName: `liff-switch-maintenance`,
+      autoDeleteObjects: true,
+    });
+
+    const distribution = new cdk.aws_cloudfront.Distribution(
+      this,
+      "WebSiteDistribution",
+      {
+        defaultRootObject: "index.html",
+        defaultBehavior: {
+          origin:
+            cdk.aws_cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
+              websiteBucket
+            ),
+        },
+      }
+    );
+
+    new cdk.CfnOutput(this, "DistributionUrl", {
+      value: `https://${distribution.distributionDomainName}`,
+    });
   }
 }
