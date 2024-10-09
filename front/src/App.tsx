69274d56360b5e827cf803a57f465f5ec81bcd99
shogo0421 <shibuya.shogo@classmethod.jp>
2024-10-09 14:15:20 +0900

    frontをliffのテンプレートに変更

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import liff from "@line/liff";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    liff
+      .init({
+        liffId: import.meta.env.VITE_LIFF_ID,
+      })
+      .then(() => {
+        setMessage("LIFF init succeeded.");
+      })
+      .catch((e: Error) => {
+        setMessage("LIFF init failed.");
+        setError(`${e}`);
+      });
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <h1>create-liff-app</h1>
+      {message && <p>{message}</p>}
+      {error && (
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <code>{error}</code>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      )}
+      <a
+        href="https://developers.line.biz/ja/docs/liff/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LIFF Documentation
+      </a>
+    </div>
+  );
 }
 
-export default App
+export default App;
